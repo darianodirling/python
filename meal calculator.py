5d02a9ac11fca4c335f8fa315efd3f2fce47936f
darianodirling <69776842+darianodirling@users.noreply.github.com>
2022-01-18 16:10:51 -0500

    Update meal calculator.py
    Updated for spring 2022 semester

--- meal calculator.py
+++ meal calculator.py
@@ -1,20 +1,20 @@
 import datetime
 
 today = datetime.date.today()
-future = datetime.date(2021,12,15)
+future = datetime.date(2022,5,5)
 diff = future - today
 #print (diff.days)
 
 weeks = diff.days / 7
 
-print('how pirate meals do you have? ')
+print('How pirate meals do you have? ')
 meals = int(input())
 
-print('how many pirate bucks do you have left? ')
+print('How many pirate bucks do you have left? ')
 bucks = int(input())
 
 MealBudget = round(meals / weeks)
 BuckBudget = round(bucks / weeks)
 
 print("This week you should use: " + str(MealBudget) + " meals" + " and " + str(BuckBudget) + " bucks")
-input('Press ENTER to exit')
+print('Press ENTER to exit the program')

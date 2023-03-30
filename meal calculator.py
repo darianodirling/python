3e5c4858df17d8d11c64cfbe6b937c38ad090da8
darianodirling <69776842+darianodirling@users.noreply.github.com>
2023-03-30 10:02:59 -0400

    Update meal calculator.py

--- meal calculator.py
+++ meal calculator.py
@@ -1,20 +1,50 @@
+
+import tkinter as tk
 import datetime
 
-today = datetime.date.today()
-future = datetime.date(2022,5,5)
-diff = future - today
-#print (diff.days)
+class PirateBudgetGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Pirate Budget")
 
-weeks = diff.days / 7
+        self.label1 = tk.Label(master, text="How many pirate meals do you have?")
+        self.label1.pack()
 
-print('How pirate meals do you have? ')
-meals = int(input())
+        self.meals_entry = tk.Entry(master)
+        self.meals_entry.pack()
 
-print('How many pirate bucks do you have left? ')
-bucks = int(input())
+        self.label2 = tk.Label(master, text="How many pirate bucks do you have left?")
+        self.label2.pack()
 
-MealBudget = round(meals / weeks)
-BuckBudget = round(bucks / weeks)
+        self.bucks_entry = tk.Entry(master)
+        self.bucks_entry.pack()
 
-print("This week you should use: " + str(MealBudget) + " meals" + " and " + str(BuckBudget) + " bucks")
-print('Press ENTER to exit the program')
+        self.calculate_button = tk.Button(master, text="Calculate", command=self.calculate_budget)
+        self.calculate_button.pack()
+
+        self.result_label = tk.Label(master, text="")
+        self.result_label.pack()
+
+        self.exit_button = tk.Button(master, text="Exit", command=master.quit)
+        self.exit_button.pack()
+
+    def calculate_budget(self):
+        today = datetime.date.today()
+        future = datetime.date(2023, 5, 1)
+        diff = future - today
+
+        weeks = diff.days / 7
+
+        meals = int(self.meals_entry.get())
+        bucks = int(self.bucks_entry.get())
+
+        meal_budget = round(meals / weeks)
+        buck_budget = round(bucks / weeks)
+
+        result = "This week you should use: {} meals and {} bucks".format(meal_budget, buck_budget)
+        self.result_label.config(text=result)
+
+# create the GUI
+root = tk.Tk()
+budget_gui = PirateBudgetGUI(root)
+root.mainloop()
